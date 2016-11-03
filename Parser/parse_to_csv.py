88c8e4a99ef091f960bc89ad9035fdfd379b9142
Ryan Hutchinson <ryan.a.hutchinson@ttu.edu>
2016-11-02 19:41:40 -0500

    Praser working
    Parses to File. Working with file in project. Will not need this in the
    future because of changed C file to create these files.

--- Parser/parse_to_csv.py
+++ Parser/parse_to_csv.py
@@ -1,20 +1,40 @@
-# This file will parse the accelerometer data from the Freedom K64 board to a csv file for use in generating visualizations
+# This file will parse the accelerometer data from the Freedom K64 board to a csv file
+#  for use in generating visualizations
+fp_g_accel = open('Test_parse.csv', 'w')  # G values for accel
+fp_g_mag = open('Test_parse_mag.csv' , 'w')
+fp_accel = open('Test_parse_accel.csv' , 'w')
+fp_mag = open('Test_mag.csv' , 'w')
 
+with open('first_drive_videoed.txt') as fp:
+    for line in fp:
+        if line.find("FXOS8700Q ACC:__ X=") != -1:
+            index_x = line.find("FXOS8700Q ACC:__ X=") + len("FXOS8700Q ACC:__ X=")
+            index_nxt = line.find("MAG: X=")
+            index_y = line.find(" Y=")
+            index_z = line.find(" Z=")
+            fp_g_accel.write(str(line[index_x:index_y]) + ",")  # X value ,
+            fp_g_accel.write(str(line[index_y + 3:index_z - 1]) + ",")  # Y value,
+            fp_g_accel.write(str(line[index_z + 3:index_nxt - 1]) + "\n")  # Z value.
+        if line.find("FXOS8700Q ACC:__ X=") != -1:
+            index_x = line.find("MAG: X=") + len("MAG: X=")
+            index_y = line.find("Y=", index_x)
+            index_z = line.find("Z=", index_y)
+            fp_g_mag.write(str(line[index_x:index_y-1]) + ",")
+            fp_g_mag.write(str(line[index_y + 3:index_z-1]) + ",")
+            fp_g_mag.write(str(line[index_z + 3:len(line)-1]) + "\n")
+        if line.find("FXOS8700Q ACC: X=") != -1:
+            index_x = line.find("FXOS8700Q ACC: X=") + len("FXOS8700Q ACC: X=")
+            index_nxt = line.find("MAG: X=")
+            index_y = line.find(" Y=")
+            index_z = line.find(" Z=")
+            fp_accel.write(str(line[index_x:index_y]) + ",")  # X value ,
+            fp_accel.write(str(line[index_y + 3:index_z - 1]) + ",")  # Y value,
+            fp_accel.write(str(line[index_z + 3:index_nxt - 1]) + "\n")  # Z value.
+        if line.find("FXOS8700Q ACC: X=") != -1:
+            index_x = line.find("MAG: X=") + len("MAG: X=")
+            index_y = line.find(" Y=", index_x)
+            index_z = line.find(" Z=", index_y)
+            fp_mag.write(str(line[index_x:index_y]) + ",")  # X value ,
+            fp_mag.write(str(line[index_y + 3:index_z - 1]) + ",")  # Y value,
+            fp_mag.write(str(line[index_z + 3:len(line) - 1]) + "\n")  # Z value.
 
-def canPush(S, state):
-    if S == []:
-        return False
-    c = S[0]
-    if state == 'empty': return alphaNum(c) or beginsSpecial(c)
-    if state == 'id': return alphaNum(c)
-    if state == 'num':
-        # don't allow push if the following two chars are both '.'
-        if len(S) > 1:
-            if S[0] == '.' and S[1] == '.':
-                return False
-        return digit(c) or c == '.'
-    if state == 'lessgreat': return c == '='
-    if state == 'colon': return c == '='
-    if state == 'period': return c == '.' or digit(c) or c == '('
-    if state == "equal": return c == '>'
-    if state == 'digitsAndPoint': return digit
